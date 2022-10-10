c84235943468216ad945cbb4e680c5e1ddf51ba9
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2022-10-10 03:01:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path = "/" element = {<Admin/>}></Route>
+      <Route path = "/dashboard" element = {<Admin/>}></Route>
       <Route path = "/overall" element = {<>Overall : Under Development</>}></Route>
       <Route path = "/employees" element = {<>Employee : Under Development</>}></Route>
       <Route path = "/today" element = {<>Employee : Under Development</>}></Route>
